13fcb566938b1aa10ef4126521768fb9526f0518
seuriseuljjeok <heeseul09@naver.com>
2024-05-11 00:49:04 +0900

    [Fix] 버튼 기본 틀 UI 수정 (#1)

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -17,192 +17,292 @@ struct ContentView: View {
             background
             
             VStack {
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Text("")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .padding()
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
+                }
                 
                 HStack {
                     Button(action: {
                         print("AC")
                     }, label: {
                         Text("AC")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.black)
                             .padding()
                             .background(.grey1)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
+                    
                     Button(action: {
                         print("/")
                     }, label: {
                         Text("/")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.black)
                             .padding()
                             .background(.grey1)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
+
                     })
+                    
                     Button(action: {
                         print("%")
                     }, label: {
                         Text("%")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.black)
                             .padding()
                             .background(.grey1)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
+                    
                     Button(action: {
                         print("^")
                     }, label: {
                         Text("^")
-                            .frame(width: 40, height: 40)
-                            .foregroundColor(.black)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
+                            .foregroundColor(.white)
                             .padding()
-                            .background(.grey1)
+                            .background(.orange)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
                 }
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 6, trailing: 0))
+                
                 HStack {
+                    
                     Button(action: {
                         print("7")
                     }, label: {
                         Text("7")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
                             .background(.grey3)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
+                    
                     Button(action: {
                         print("8")
                     }, label: {
                         Text("8")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
                             .background(.grey3)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
+                    
                     Button(action: {
                         print("9")
                     }, label: {
                         Text("9")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
                             .background(.grey3)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
+                    
                     Button(action: {
                         print("X")
                     }, label: {
                         Text("X")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
-                            .background(.grey3)
+                            .background(.orange)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
                 }
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 6, trailing: 0))
+                
                 HStack {
+                    
                     Button(action: {
                         print("4")
                     }, label: {
                         Text("4")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
                             .background(.grey3)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
+                    
                     Button(action: {
                         print("5")
                     }, label: {
                         Text("5")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
                             .background(.grey3)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
+                    
                     Button(action: {
                         print("6")
                     }, label: {
                         Text("6")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
                             .background(.grey3)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
+                    
                     Button(action: {
                         print("-")
                     }, label: {
                         Text("-")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
-                            .background(.grey3)
+                            .background(.orange)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
                 }
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 6, trailing: 0))
+
+                
                 HStack {
                     Button(action: {
                         print("1")
                     }, label: {
                         Text("1")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
                             .background(.grey3)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
+                    
                     Button(action: {
                         print("2")
                     }, label: {
                         Text("2")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
                             .background(.grey3)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
+                    
                     Button(action: {
                         print("3")
                     }, label: {
                         Text("3")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
                             .background(.grey3)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
                     })
+                    
                     Button(action: {
                         print("+")
                     }, label: {
                         Text("+")
-                            .frame(width: 40, height: 40)
+                            .frame(width: 50, height: 50)
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(.orange)
+                            .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
+                    })
+                }
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 6, trailing: 0))
+                
+                HStack {
+                    
+                    Button(action: {
+                        print("0")
+                    }, label: {
+                        Text("0")
+                            .frame(width: 142, height: 50)
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(.grey3)
+                            .clipShape(RoundedRectangle(cornerRadius: 70))
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
+                    })
+                    
+                    Button(action: {
+                        print(".")
+                    }, label: {
+                        Text(".")
+                            .frame(width: 50, height: 50)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
                             .background(.grey3)
                             .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
+
                     })
+                    
+                    Button(action: {
+                        print("=")
+                    }, label: {
+                        Text("=")
+                            .frame(width: 50, height: 50)
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(.orange)
+                            .clipShape(Circle())
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 6))
+                    })
+                    
                 }
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 80, trailing: 0))
             }
-            .padding()
         }
         .ignoresSafeArea()
-        
     }
     
-}
-
-struct CustomButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .foregroundColor(.blue)
-            .padding() // 예시로 추가된 스타일
-            .background(Color.gray) // 예시로 추가된 스타일
-            .cornerRadius(8) // 예시로 추가된 스타일
-    }
 }
 
 #Preview {
